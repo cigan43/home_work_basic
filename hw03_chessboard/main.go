fbf864ccbb773a2d7154813753a6831a658b72e2
abagaev <abagaev@bank-hlynov.ru>
2023-12-25 16:58:00 +0300

    hw03_chessboard

--- hw03_chessboard/main.go
+++ hw03_chessboard/main.go
@@ -12,18 +12,15 @@ const (
 func CreateString(countColumn int, firstStep int) string {
 	var readyString string
 	for i := 0; i < countColumn; i++ {
-		if firstStep%2 == 0 {
-			if len(readyString)%2 == 0 {
-				readyString += black
-			} else {
-				readyString += white
-			}
-		} else {
-			if len(readyString)%2 == 0 {
-				readyString += white
-			} else {
-				readyString += black
-			}
+		if len(readyString)%2 == 0 && firstStep%2 == 0 {
+			readyString += black
+		} else if len(readyString)%2 != 0 && firstStep%2 == 0 {
+			readyString += white
+		}
+		if len(readyString)%2 == 0 && firstStep%2 != 0 {
+			readyString += white
+		} else if len(readyString)%2 != 0 && firstStep%2 != 0 {
+			readyString += black
 		}
 	}
 	return readyString
@@ -35,7 +32,7 @@ func main() {
 	fmt.Scanf("%dx%d", &row, &column)
 
 	for i := 0; row > i; i++ {
-		board = board + "\n" + CreateString(column, i)
+		board += "\n" + CreateString(column, i)
 	}
 	fmt.Println(board)
 }
