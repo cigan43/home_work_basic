b12904e0635fbda2446ef54820c2fe215d46501f
abagaev <abagaev@bank-hlynov.ru>
2024-01-10 17:08:28 +0300

    начало

--- hw04_struct_comparator/main.go
+++ hw04_struct_comparator/main.go
@@ -1,5 +1,25 @@
 package main
 
+type Book struct {
+	id     uint
+	title  string
+	author string
+	year   int
+	size   int
+	rate   float32
+}
+
+func newBook(id uint, title, author string, year, size int, rate float32) *Book {
+	return &Book{
+		id:     id,
+		title:  title,
+		author: author,
+		year:   year,
+		size:   size,
+		rate:   rate,
+	}
+}
+
 func main() {
-	// Place your code here.
+	p := newBook(1, "Война и мир", "Толстой", 2020, 500, 5.8)
 }
