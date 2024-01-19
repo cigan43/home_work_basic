0d19094e21778efccf5c3bf40c7a3b26a75061af
cigan43 <cigan43@gmail.com>
2024-01-20 01:54:01 +0300

    addув test for chessboard

---
+++ hw06_testing/chessboard/main_test.go
@@ -0,0 +1,45 @@
+package chessboard
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func Test(t *testing.T) {
+	testCases := []struct {
+		desc, want             string
+		countColumn, firstStep int
+	}{
+		{
+			desc:        "OneBlack",
+			countColumn: 8,
+			firstStep:   0,
+			want:        "# # # # ",
+		},
+		{
+			desc:        "OneWhite",
+			countColumn: 9,
+			firstStep:   1,
+			want:        " # # # # ",
+		},
+		{
+			desc:        "ErrorOneWhite",
+			countColumn: 5,
+			firstStep:   1,
+			want:        "# # #",
+		},
+		{
+			desc:        "ErrorString",
+			countColumn: "34b",
+			firstStep:   1,
+			want:        "# # #",
+		},
+	}
+	for _, tC := range testCases {
+		t.Run(tC.desc, func(t *testing.T) {
+			got := CreateString(tC.countColumn, tC.firstStep)
+			assert.Equal(t, tC.want, got)
+		})
+	}
+}
