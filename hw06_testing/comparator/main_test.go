30dfea141898483ea2e22c403ffbdc3e42db6090
abagaev <abagaev@bank-hlynov.ru>
2024-01-23 16:42:32 +0300

    добавил тестирование для модуля comparator

--- hw06_testing/comparator/main_test.go
+++ hw06_testing/comparator/main_test.go
@@ -1,22 +1,30 @@
 package comparator
 
-import "testing"
+import (
+	"testing"
+
+	// "github.com/cigan43/home_work_basic/hw06_testing/comparator"
+	"github.com/stretchr/testify/assert"
+)
 
 func Test(t *testing.T) {
 	testCases := []struct {
 		desc    string
 		book1   Book
 		book2   Book
-		compare int
+		compare FieldComapre
 		want    bool
 	}{
-		{
-			desc: "",
-		},
+		{"Good", Book{1, "War and peace", "Tolstoi", 1879, 1000, 8}, Book{2, "Python", "Lics", 2023, 300, 5}, 2, true},
+		{"Good1", Book{1, "War and peace", "Tolstoi", 1879, 1000, 8}, Book{2, "Python", "Lics", 2023, 300, 5}, 1, true},
+		{"Good2", Book{1, "War and peace", "Tolstoi", 1879, 1000, 8}, Book{2, "Python", "Lics", 2023, 300, 5}, 3, false},
 	}
 	for _, tC := range testCases {
 		t.Run(tC.desc, func(t *testing.T) {
 
+			got := NewComparator(tC.compare)
+			result := got.Compare
+			assert.Equal(t, result(&tC.book1, &tC.book2), tC.want)
 		})
 	}
 }
