3f36f957e9c722d75590297212afedd1caa92335
abagaev <abagaev@bank-hlynov.ru>
2024-01-30 17:15:49 +0300

    пока черновик

--- hw07_word_counter/main.go
+++ hw07_word_counter/main.go
@@ -1,5 +1,33 @@
 package main
 
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
+func WordSplit(row string) []string {
+
+	splitFunc := func(r rune) bool {
+		return strings.ContainsRune("*%,_!.' '", r)
+	}
+
+	words := strings.FieldsFunc(text, splitFunc)
+	for idx, word := range words {
+		fmt.Printf("Word %d is: %s\n", idx, word)
+	}
+
+}
+
 func main() {
-	// Place your code here.
+	text, _ := bufio.NewReader(os.Stdin).ReadString('\n')
+	splitFunc := func(r rune) bool {
+		return strings.ContainsRune("*%,_!.' '", r)
+	}
+
+	words := strings.FieldsFunc(text, splitFunc)
+	for idx, word := range words {
+		fmt.Printf("Word %d is: %s\n", idx, word)
+	}
 }
