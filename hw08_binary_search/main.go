d81dd330b1a32349ac62d431daa355b07510c73d
cigan43 <cigan43@gmail.com>
2024-02-03 00:05:22 +0300

    пока набросок

--- hw08_binary_search/main.go
+++ hw08_binary_search/main.go
@@ -1,5 +1,25 @@
 package main
 
+import (
+	"fmt"
+	"sort"
+)
+
+type Range struct {
+	min, max, num int
+	nums          []int
+}
+
+func sortSlice(ar []int) []int {
+	sort.Slice(ar, func(i, j int) bool {
+		return ar[i] < ar[j]
+	})
+	return ar
+}
+
 func main() {
-	// Place your code here.
+	a := []int{1, 2, 5, 7, 3, 4, 6, 9, 8}
+	// searchIndex := 5
+	s := sortSlice(a)
+	fmt.Println(s)
 }
