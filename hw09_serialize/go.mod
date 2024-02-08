9c4c81691b6f3d378281b2a586021b54fadac005
abagaev <abagaev@bank-hlynov.ru>
2024-02-08 12:52:04 +0300

    templates

--- hw09_serialize/go.mod
+++ hw09_serialize/go.mod
@@ -1,3 +1,3 @@
-module github.com/fixme_my_friend/hw09_serialize
+module github.com/cigan43/home_work_basic/hw09_serialize
 
 go 1.20
--- hw09_serialize/main.go
+++ hw09_serialize/main.go
@@ -1,5 +1,22 @@
 package main
 
+type Book struct {
+	ID     int64   `json:"id"`
+	Title  string  `json:"title"`
+	Author string  `json:"author"`
+	Year   int     `json:"year"`
+	Size   int     `json:"size"`
+	Rate   float64 `json:"rate"`
+}
+
+
+type 
+
+
+type Message interface{
+
+}
+
 func main() {
 	// Place your code here.
 }
