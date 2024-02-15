05583770d231e3855cd5dec5197fc0f6d6961b26
abagaev <abagaev@bank-hlynov.ru>
2024-02-15 11:27:29 +0300

    пока шаблон

--- hw09_serialize/main.go
+++ hw09_serialize/main.go
@@ -1,5 +1,14 @@
 package main
 
+import "encoding/json"
+
+// - Реализуйте структуру Book со следующими полями: ID, Title, Author, Year, Size, Rate (может быть дробным).
+// - Реализуйте для нее интерфейсы Marshaller и Unmarshaller  из пакета json.
+// - Составьте protobuf спецификацию для Book
+// - Реализуйте для нее интерфейс Message из пакета proto.
+// - Напишите фукции выполняющие сериализацию/десериализацию слайса объектов.
+// - Напишите юнит тесты на реализованные функции;
+
 type Book struct {
 	ID     int64   `json:"id"`
 	Title  string  `json:"title"`
@@ -9,12 +18,31 @@ type Book struct {
 	Rate   float64 `json:"rate"`
 }
 
+type Marshaller interface {
+	MarshalJSON() ([]byte, error)
+}
 
-type 
+type Unmarshaller interface {
+	UnmarshalJSON([]byte) error
+}
 
+func (b *Book) MarshalJSON() ([]byte, error) {
+	bookMarshal, err := json.Marshal(b)
+	if err != nil {
+		return nil, err
+	}
+	return bookMarshal, nil
+}
 
-type Message interface{
+func (b *Book) UnmarshalJSON(bytes []byte) error {
+	err := json.Unmarshal(bytes, b)
+	if err != nil {
+		return err
+	}
+	return nil
+}
 
+type Message interface {
 }
 
 func main() {
