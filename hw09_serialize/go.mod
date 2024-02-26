2ac5b34c83cb71ce4e64a8f3406fd44690ef1e18
cigan43 <cigan43@gmail.com>
2024-02-26 09:36:21 +0300

    пока не рабочая хрень

--- hw09_serialize/go.mod
+++ hw09_serialize/go.mod
@@ -2,4 +2,9 @@ module github.com/cigan43/home_work_basic/hw09_serialize
 
 go 1.20
 
-require google.golang.org/protobuf v1.32.0
+require (
+	go.starlark.net v0.0.0-20240123142251-f86470692795
+	google.golang.org/protobuf v1.32.0
+)
+
+require golang.org/x/sys v0.0.0-20220715151400-c0bba94af5f8 // indirect
--- hw09_serialize/main.go
+++ hw09_serialize/main.go
@@ -2,8 +2,8 @@ package main
 
 import (
 	"encoding/json"
-
-	"google.golang.org/protobuf/proto"
+	"fmt"
+	// "google.golang.org/protobuf/proto"
 )
 
 // - Реализуйте структуру Book со следующими полями: ID, Title, Author, Year, Size, Rate (может быть дробным).
@@ -17,8 +17,8 @@ type Book struct {
 	ID     int64   `json:"id"`
 	Title  string  `json:"title"`
 	Author string  `json:"author"`
-	Year   int     `json:"year"`
-	Size   int     `json:"size"`
+	Year   int64   `json:"year"`
+	Size   int64   `json:"size"`
 	Rate   float64 `json:"rate"`
 }
 
@@ -31,10 +31,13 @@ type Unmarshaller interface {
 }
 
 func (b *Book) MarshalJSON() ([]byte, error) {
+	fmt.Println(b)
 	bookMarshal, err := json.Marshal(b)
+	fmt.Println(err, "-------")
 	if err != nil {
 		return nil, err
 	}
+	fmt.Println(bookMarshal, "+++")
 	return bookMarshal, nil
 }
 
@@ -46,36 +49,71 @@ func (b *Book) UnmarshalJSON(bytes []byte) error {
 	return nil
 }
 
-type MessageUnmarshaler interface {
-	MessageUnmarshaler([]byte) error
-}
+// type MessageUnmarshaler interface {
+// 	MessageUnmarshaler([]byte) error
+// }
 
-type MessageMarshaler interface {
-	MessageMarshaler() ([]byte, error)
-}
+// type MessageMarshaler interface {
+// 	MessageMarshaler() ([]byte, error)
+// }
 
-func (b *Book) MessageMarshaler() ([]byte, error) {
-	bookMarshal, err := proto.Marshal(b)
-	if err != nil {
-		return nil, err
+// func (b *Book) MessageMarshaler() ([]byte, error) {
+// 	bookMarshal, err := proto.Marshal(b)
+// 	if err != nil {
+// 		return nil, err
+// 	}
+// 	return bookMarshal, nil
+// }
+
+// func (b *Book) UnmarshalJSON(bytes []byte) error {
+// 	return proto.Unmarshal(bytes, b)
+// }
+
+func SliceBookMarshel(ss []Book) []byte {
+	var bookbyte []byte
+	for i := range ss {
+		a, err := ss[i].MarshalJSON()
+		if err != nil {
+			fmt.Println(err)
+		}
+		fmt.Println(a)
+		// append(bookbyte, )
 	}
-	return bookMarshal, nil
+	return bookbyte
 }
 
-func (b *Book) UnmarshalJSON(bytes []byte) error {
-	return proto.Unmarshal(bytes, b)
-}
-
-func SliceBook(ss []Book)
+// func SliceBookUnMarshel(ss []Book) []byte {
+// 	var b []Book
+// 	bookbyte := []byte
+// 	for i := range ss {
+// 		fmt.Println(i)
+// 		// append(bookbyte, i.UnmarshalJSON(byte(i), b))
+// 	}
+// 	return bookbyte
+// }
 
 func main() {
-	myBook := &Book{
-		ID:     1,
-		Title:  "Pupkin",
-		Author: "Pup",
-		Year:   2000,
-		Size:   300,
-		Rate:   6.7,
+	myBook := []Book{
+		{
+			ID:     1,
+			Title:  "Pupkin",
+			Author: "Pup",
+			Year:   2000,
+			Size:   300,
+			Rate:   6.7,
+		},
+		{
+			ID:     2,
+			Title:  "Rfr",
+			Author: "kfd;sk",
+			Year:   200,
+			Size:   100,
+			Rate:   5.9,
+		},
 	}
-	a := myBook.MarshalJSON()
+
+	a := SliceBookMarshel(myBook)
+	// a := SliceBookUnMarshel(myBook)
+	fmt.Println(a)
+	// proto.Message()
 }
