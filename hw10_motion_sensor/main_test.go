f56a200f989819fd3229587ec2fce9d4522a03ad
abagaev <abagaev@bank-hlynov.ru>
2024-03-14 16:56:16 +0300

    add test

---
+++ hw10_motion_sensor/main_test.go
@@ -0,0 +1,19 @@
+func func TestAVG(t *testing.T) {
+	testCases := []struct {
+		desc	string
+		chanIn chan float32
+		chanOut float32
+		want float32
+	}{
+		{
+			desc: "GOOD",
+			chanIn: 
+			
+		},
+	}
+	for _, tC := range testCases {
+		t.Run(tC.desc, func(t *testing.T) {
+			
+		})
+	}
+}
