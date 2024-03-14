a6a42891a5f6799f06283386e3589fa0f766ea5e
cigan43 <cigan43@gmail.com>
2024-03-15 00:55:58 +0300

    end

--- hw10_motion_sensor/main.go
+++ hw10_motion_sensor/main.go
@@ -1,27 +1,25 @@
 package main
 
 import (
+	"crypto/rand"
 	"fmt"
-	"math/rand"
+	"math/big"
 	"time"
 )
 
-// - Создайте горутину для имитации считывания данных с сенсора и передачи их в канал. Данные должны считываться в течение 1 минуты.
-// - Создайте горутину для обработки данных. Для каждых 10 полученных значений вычисляется среднее арифметическое и отправляется в канал с обработанными данными.
-// - Главная горутина будет получать обработанные данные из канала и выводить их на экран.
-// - Напишите юнит тесты на реализованные функции;
-
-func genData(gChannel chan int) {
+func genData(gChannel chan int, limit int64) {
 	t := time.Now()
 	for {
-		// fmt.Println(time.Since(t).Seconds(), "--------")
 		if time.Since(t).Seconds() > 60.0 {
 			break
 		}
-		gChannel <- rand.Intn(1000)
+		intRand, err := rand.Int(rand.Reader, big.NewInt(limit))
+		if err != nil {
+			return
+		}
+		gChannel <- int(intRand.Int64())
 	}
 	close(gChannel)
-
 }
 
 func aVg(inChan <-chan int, outChan chan float32) {
@@ -33,21 +31,19 @@ func aVg(inChan <-chan int, outChan chan float32) {
 			count = 0
 		} else {
 			data += i
-			count += 1
+			count++
 		}
 	}
 	close(outChan)
-
 }
 
 func main() {
 	gChan := make(chan int)
 	counted := make(chan float32)
-	go genData(gChan)
+	go genData(gChan, 1000)
 	go aVg(gChan, counted)
 
 	for b := range counted {
-		fmt.Println(b, "!!!!!!!")
+		fmt.Println(b)
 	}
-
 }
--- hw10_motion_sensor/main_test.go
+++ hw10_motion_sensor/main_test.go
@@ -1,19 +1,32 @@
-func func TestAVG(t *testing.T) {
+package main
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestGenData(t *testing.T) {
 	testCases := []struct {
-		desc	string
-		chanIn chan float32
-		chanOut float32
-		want float32
+		desc  string
+		limit int64
+		want  bool
 	}{
 		{
-			desc: "GOOD",
-			chanIn: 
-			
+			desc:  "GOOD",
+			limit: 20.0,
+			want:  true,
 		},
 	}
 	for _, tC := range testCases {
 		t.Run(tC.desc, func(t *testing.T) {
-			
+			testChan := make(chan int)
+			go genData(testChan, tC.limit)
+			_, ok := <-testChan
+			if !ok {
+				assert.Equal(t, tC.want, false)
+			}
+			assert.Equal(t, tC.want, ok)
 		})
 	}
 }
