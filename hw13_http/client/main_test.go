5017851f6ae3af8a885daa0f0a6cb8ff7570ae41
abagaev <abagaev@bank-hlynov.ru>
2024-04-23 17:03:19 +0300

    add test

--- hw13_http/client/main_test.go
+++ hw13_http/client/main_test.go
@@ -10,31 +10,31 @@ func Test(t *testing.T) {
 	testCases := []struct {
 		desc    string
 		Address string
-		Url     string
+		URL     string
 		Data    []byte
 		want    string
 	}{
 		{
 			desc:    "Post",
 			Address: "127.0.0.1:8000",
-			Url:     "post",
+			URL:     "post",
 			Data:    []byte(`{"Name":"John", "LastName": "Pup", "Age":30}`),
 			want:    "200 ok",
 		},
 		{
 			desc:    "get",
 			Address: "127.0.0.1:8000",
-			Url:     "get",
+			URL:     "get",
 			want:    "200 ok",
 		},
 	}
 	for _, tC := range testCases {
 		t.Run(tC.desc, func(t *testing.T) {
-			if tC.Url == "post" {
-				status, _ := clientPost(tC.Address, tC.Url, tC.Data)
+			if tC.URL == "post" {
+				status, _ := clientPost(tC.Address, tC.URL, tC.Data)
 				assert.Equal(t, tC.want, status)
 			} else {
-				status, _ := clientGet(tC.Address, tC.Url)
+				status, _ := clientGet(tC.Address, tC.URL)
 				assert.Equal(t, tC.want, status)
 			}
 		})
